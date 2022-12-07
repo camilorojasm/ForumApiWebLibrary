522a86657ebde457432b90bfc924f3c0e4d80796
Steve Collins <steve@ovenbot.com>
2022-12-07 10:03:29 -0500

    Latest changes

--- ForumApiWebLibrary/Client/Pages/Posts.razor.cs
+++ ForumApiWebLibrary/Client/Pages/Posts.razor.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ForumApiWebLibrary.Client.Pages
 {
@@ -9,5 +10,31 @@ namespace ForumApiWebLibrary.Client.Pages
     {
         [Parameter]
         public long TId { get; set; }
+
+        public int ActiveTabIndex { get; set; } = 0;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+
+        async Task TabChangedHandler(int newIndex)
+        {
+            ActiveTabIndex = newIndex;
+
+            //switch (ActiveTabIndex)
+            //{
+            //    case 0:
+            //        TopicsModel = await ForumApiClient.GetTopicsActiveViewAsync(FId, 1, PageSize);
+            //        break;
+            //    case 1:
+            //        TopicsModel = await ForumApiClient.GetTopicsRecentViewAsync(FId, 1, PageSize);
+            //        break;
+            //    case 2:
+            //        TopicsModel = await ForumApiClient.GetTopicsUpCountViewAsync(FId, 1, PageSize);
+            //        break;
+            //}
+
+        }
     }
 }
