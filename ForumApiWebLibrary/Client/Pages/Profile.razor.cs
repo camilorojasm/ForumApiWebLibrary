42370ce81acb5707854fd13776484df1ebaf0388
Steve Collins <steve@ovenbot.com>
2022-12-12 13:14:50 -0500

    Added Nuger.config

--- ForumApiWebLibrary/Client/Pages/Profile.razor.cs
+++ ForumApiWebLibrary/Client/Pages/Profile.razor.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using ForumApiDataService.Client;
+using ForumApiDataService.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Telerik.Blazor.Components;
 
 namespace ForumApiWebLibrary.Client.Pages
 {
     public partial class Profile : ComponentBase
     {
+        public UserProfileModel ProfileModel { get; set; } = new UserProfileModel();
+
+        List<string> AllowedExtensions { get; set; } = new List<string>() { ".docx", ".pdf" };
+
+        int MaxFileSize { get; set; } = 1024 * 1024; // 1 MB
+
+        async Task OnSelectHandler(FileSelectEventArgs args)
+        {
+            foreach (var file in args.Files)
+            {
+                var buffer = new byte[file.Stream.Length];
+                await file.Stream.ReadAsync(buffer);
+            }
+        }
+        private async Task FetchData()
+        {
+            ProfileModel = await ForumApiClient.GetUserProfileViewAsync();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await FetchData();
+            await base.OnInitializedAsync();
+        }
+
+        public bool ValidSubmit { get; set; } = false;
+
+        async void HandleValidSubmit()
+        {
+            ValidSubmit = true;
+
+            var userEntry = new UserEntryModel()
+            {
+                UserName = ProfileModel.UserName,
+                EmailAddress = ProfileModel.EmailAddress,
+                OptinInternal = ProfileModel.OptinInternal,
+                OptinExternal = ProfileModel.OptinExternal
+            };
+
+            var response = await ForumApiClient.PutAccountAsync(userEntry);
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.NoContent:
+                    await FetchData();
+                    break;
+                case HttpStatusCode.BadRequest:
+                    {
+                        string res = await response.Content.ReadAsStringAsync();
+                        Logger.LogInformation(res);
+                    }
+                    break;
+                case HttpStatusCode.Unauthorized:
+                    await ForumAuthService.Logout();
+                    NavManager.NavigateTo("/login");
+                    break;
+                default:
+                    return;
+            }
+
+            ValidSubmit = false;
+
+            StateHasChanged();
+        }
+
+        void HandleInvalidSubmit()
+        {
+            ValidSubmit = false;
+        }
 
     }
 }
--- ForumApiWebLibrary/Client/Pages/Register.razor.cs
+++ ForumApiWebLibrary/Client/Pages/Register.razor.cs
@@ -1,19 +1,20 @@
-﻿using ForumApiDataService.Models;
+﻿using ForumApiDataService.Client;
+using ForumApiDataService.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Telerik.Blazor.Components.Grid;
 
 namespace ForumApiWebLibrary.Client.Pages
 {
     public partial class Register : ComponentBase
     {
-        public RegistrationModel SignUpUser { get; set; } = new RegistrationModel()
-        {
-            //Email = "johny.doe@email.com",
-            //Password = "testpassword",
-        };
+        public RegistrationModel SignUpUser { get; set; } = new RegistrationModel();
 
         public bool ValidSubmit { get; set; } = false;
 
@@ -21,7 +22,29 @@ namespace ForumApiWebLibrary.Client.Pages
         {
             ValidSubmit = true;
 
-            await Task.Delay(2000);
+            //Assign claims
+            SignUpUser.Claims = new List<ClaimModel> { new ClaimModel() { Claim = "Member" } };
+
+            var response = await ForumApiClient.PostUserRegistrationAsync(SignUpUser);
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.Created:
+                    NavManager.NavigateTo("/login");
+                    break;
+                case HttpStatusCode.BadRequest:
+                    {
+                        string res = await response.Content.ReadAsStringAsync();
+                        Logger.LogInformation(res);
+                    }
+                    break;
+                case HttpStatusCode.Unauthorized:
+                    await ForumAuthService.Logout();
+                    NavManager.NavigateTo("/login");
+                    break;
+                default:
+                    return;
+            }
 
             ValidSubmit = false;
 
